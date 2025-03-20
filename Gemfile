cdb4f851aa714fe9eea4481d3977fdf854dad334
Seth Herr <seth.william.herr@gmail.com>
2025-03-20 10:59:35 -0700

    tests

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ source "https://rubygems.org"
 
 ruby "3.4.2"
 
+gem "csv"
 gem "rspec" # tests
 gem "standard" # Ruby linter
--- tests.rb
+++
@@ -1,9 +0,0 @@
-require "rspec"
-require "csv"
-
-# Download a copy from Bike Index to compare against the updates in the repo
-system("bin/download_from_bike_index.sh tmp/manufacturers.csv", exception: true)
-
-RSpec.describe "manufacturers.csv" do
-  it "has at least as many manufacturers as are on bikeindex"
-end
