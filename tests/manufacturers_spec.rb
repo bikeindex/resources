dbc6304190f3b983eb1d4bc5c4dbe77adbf6284b
Seth Herr <seth.william.herr@gmail.com>
2025-03-20 11:50:44 -0700

    lint

--- tests/manufacturers_spec.rb
+++ tests/manufacturers_spec.rb
@@ -2,7 +2,7 @@ require "rspec"
 require "csv"
 
 # Set the application root directory
-APP_ROOT = File.expand_path('..', __dir__)
+APP_ROOT = File.expand_path("..", __dir__)
 
 # Download a copy from Bike Index to compare against the updates in the repo
 system("bin/download_from_bike_index tmp/manufacturers.csv", exception: true)
@@ -10,11 +10,11 @@ system("bin/download_from_bike_index tmp/manufacturers.csv", exception: true)
 BIKE_INDEX_CSV = CSV.read("tmp/manufacturers.csv", headers: true, header_converters: :symbol)
 REPO_CSV =
 
-RSpec.describe "manufacturers.csv" do
-  let(:repo_csv) { CSV.read("manufacturers.csv", headers: true, header_converters: :symbol) }
+  RSpec.describe "manufacturers.csv" do
+    let(:repo_csv) { CSV.read("manufacturers.csv", headers: true, header_converters: :symbol) }
 
-  it "has at least as many manufacturers as are on bikeindex, and the same headers" do
-    expect(repo_csv.count).to eq BIKE_INDEX_CSV.count
-    expect(repo_csv.headers).to eq BIKE_INDEX_CSV.headers
+    it "has at least as many manufacturers as are on bikeindex, and the same headers" do
+      expect(repo_csv.count).to eq BIKE_INDEX_CSV.count
+      expect(repo_csv.headers).to eq BIKE_INDEX_CSV.headers
+    end
   end
-end
