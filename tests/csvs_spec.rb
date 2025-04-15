da9e61205e8119b52a927cb8f3cd4d21cf5f915b
Seth Herr <seth.william.herr@gmail.com>
2025-04-14 19:18:45 -0700

    add primary activity

---
+++ tests/csvs_spec.rb
@@ -0,0 +1,20 @@
+require "rspec"
+require "csv"
+
+# Set the application root directory
+APP_ROOT = File.expand_path("..", __dir__)
+
+# Download a copy from Bike Index to compare against the updates in the repo
+system("bin/download_from_bike_index tmp/", exception: true)
+
+BIKE_INDEX_MANUFACTURERS_CSV = CSV.read("tmp/manufacturers.csv", headers: true, header_converters: :symbol)
+BIKE_INDEX_ACTIVITIES_CSV = CSV.read("tmp/primary_activities.csv", headers: true, header_converters: :symbol)
+
+RSpec.describe "manufacturers.csv" do
+  let(:repo_csv) { CSV.read("manufacturers.csv", headers: true, header_converters: :symbol) }
+
+  it "has at least as many manufacturers as are on bikeindex, and the same headers" do
+    expect(repo_csv.count).to eq BIKE_INDEX_MANUFACTURERS_CSV.count
+    expect(repo_csv.headers).to eq BIKE_INDEX_MANUFACTURERS_CSV.headers
+  end
+end
