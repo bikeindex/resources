5d8e0d36f2fd9eb9494b408770f45204bc6f54f3
Seth Herr <seth.william.herr@gmail.com>
2025-04-14 20:55:47 -0700

    add a test

--- tests/csvs_spec.rb
+++ tests/csvs_spec.rb
@@ -10,11 +10,22 @@ system("bin/download_from_bike_index tmp/", exception: true)
 BIKE_INDEX_MANUFACTURERS_CSV = CSV.read("tmp/manufacturers.csv", headers: true, header_converters: :symbol)
 BIKE_INDEX_ACTIVITIES_CSV = CSV.read("tmp/primary_activities.csv", headers: true, header_converters: :symbol)
 
-RSpec.describe "manufacturers.csv" do
-  let(:repo_csv) { CSV.read("manufacturers.csv", headers: true, header_converters: :symbol) }
+RSpec.describe "CSVs" do
+  describe "manufacturers.csv" do
+    let(:repo_csv) { CSV.read("manufacturers.csv", headers: true, header_converters: :symbol) }
 
-  it "has at least as many manufacturers as are on bikeindex, and the same headers" do
-    expect(repo_csv.count).to eq BIKE_INDEX_MANUFACTURERS_CSV.count
-    expect(repo_csv.headers).to eq BIKE_INDEX_MANUFACTURERS_CSV.headers
+    it "has at least as many manufacturers as are on bikeindex, and the same headers" do
+      expect(repo_csv.count).to eq BIKE_INDEX_MANUFACTURERS_CSV.count
+      expect(repo_csv.headers).to eq BIKE_INDEX_MANUFACTURERS_CSV.headers
+    end
+  end
+
+  describe "primary_activities.csv" do
+    let(:repo_csv) { CSV.read("primary_activities.csv", headers: true, header_converters: :symbol) }
+
+    it "has at least as many manufacturers as are on bikeindex, and the same headers" do
+      expect(repo_csv.count).to eq BIKE_INDEX_ACTIVITIES_CSV.count
+      expect(repo_csv.headers).to eq BIKE_INDEX_ACTIVITIES_CSV.headers
+    end
   end
 end
